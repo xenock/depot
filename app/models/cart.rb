e7b0bcbcb1a4a2b5b42fe00c11349d2470b3fb36
Xenock <miguelmoracho@gmail.com>
2015-07-01 03:58:45 +0200

    Added validation to Cart model before_destroy

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
