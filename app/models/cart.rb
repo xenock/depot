15f4d96a181464222e0deb025de8f959049f659b
Xenock <miguelmoracho@gmail.com>
2015-07-03 18:48:59 +0200

    Modified controller and template to add product quantity in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,4 +10,14 @@ class Cart < ActiveRecord::Base
       return false
     end
   end
+
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
 end
