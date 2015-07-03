f178a3a0ee8b34ccf9e464aa2ab678d176c316ea
Xenock <miguelmoracho@gmail.com>
2015-07-03 18:56:09 +0200

    Added migration to clean duplicated items in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703163649) do
+ActiveRecord::Schema.define(version: 20150703165043) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
