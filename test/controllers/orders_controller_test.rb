038ff997066fa26cf6964a47a73ab24b8ae3e820
Xenock <miguelmoracho@gmail.com>
2015-07-27 22:26:33 +0200

    Adapted default controller and tests for desired behavior

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -12,8 +12,19 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
     get :new
     assert_response :success
+  end
+
+  test "require items in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], "Your cart is empty"
   end
 
   test "should create order" do
