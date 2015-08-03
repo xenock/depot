0038102949037005f7b64e67dd3f3e6597065755
Xenock <miguelmoracho@gmail.com>
2015-08-03 21:02:04 +0200

    modified templates, partials and mailer

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -5,10 +5,10 @@ class OrderNotifier < ApplicationMailer
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
