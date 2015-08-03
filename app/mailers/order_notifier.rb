0a7c9f02113d56a28746e8e9dc36160798c9cfbf
Xenock <miguelmoracho@gmail.com>
2015-08-03 20:55:56 +0200

    added default from to class

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ApplicationMailer
-
+  default from: "Sam Ruby <depot@example.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
