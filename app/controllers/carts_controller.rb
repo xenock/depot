8499274c59162ddfc32d564c1c20bce3a1cb5c93
Xenock <miguelmoracho@gmail.com>
2015-08-12 23:37:22 +0200

    limiting access. authorizing

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
@@ -30,9 +31,9 @@ class CartsController < ApplicationController
     respond_to do |format|
       if @cart.save
         format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render :show, status: :created, location: @cart }
+        format.json { render action: 'show', status: :created, location: @cart }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
@@ -44,9 +45,9 @@ class CartsController < ApplicationController
     respond_to do |format|
       if @cart.update(cart_params)
         format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
@@ -58,24 +59,23 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'Your cart is currently empty.' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_cart
       @cart = Cart.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
     end
 
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_url, notice: 'Invalid Cart'
+      redirect_to store_url, notice: 'Invalid cart'
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,20 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  ActiveRecord::Migration.check_pending!
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def login_as(user)
+    session[:user_id] = users(user).id
+  end
+
+  def logout
+    session.delete :user_id
+  end
+
+  def setup
+    login_as :one if defined? session
+  end
 end
