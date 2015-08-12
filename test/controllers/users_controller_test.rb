1f6cfb62676ee2820304e5430dc659e08fd17584
Xenock <miguelmoracho@gmail.com>
2015-08-12 18:44:18 +0200

    updated user controller and its test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,10 +18,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
+      post :create, user: { name: 'sam', password: 'secret', password_confirmation: 'secret' }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
@@ -35,8 +35,8 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
-    assert_redirected_to user_path(assigns(:user))
+    patch :update, id: @user, user: { name: 'sam', password: 'secret', password_confirmation: 'secret' }
+    assert_redirected_to users_path
   end
 
   test "should destroy user" do
