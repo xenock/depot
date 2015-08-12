ad7f8bb2e4c9be5b487765d6a2a900b8066d763b
Xenock <miguelmoracho@gmail.com>
2015-08-12 18:03:25 +0200

    moved mailer block from development config to general config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,16 +39,3 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
-
-Depot::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:          'smtp.gmail.com',
-    port:             587,
-    domain:           'domain.of.sender.net',
-    authentication:   'plain',
-    user_name:        'dave',
-    password:         'secret',
-    enable_starttls_auto: true
-  }
-end
