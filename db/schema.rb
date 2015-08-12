81ca4ff9711576a4d04a01549c8fa4dbea094d44
Xenock <miguelmoracho@gmail.com>
2015-08-12 18:10:26 +0200

    scaffolded User and migration done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727200927) do
+ActiveRecord::Schema.define(version: 20150812160713) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20150727200927) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
