5d6f0d24f6f60e45adffc81c3b74f97999803732
Xenock <miguelmoracho@gmail.com>
2015-09-01 02:16:56 +0200

    Updated flash message in the orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver_now
 
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render action :show, status: :created, location: @order }
       else
         format.html { render :new }
